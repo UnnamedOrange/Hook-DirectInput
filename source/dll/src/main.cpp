c3e4a93b68833db92c2e1c59597f18b71860be69
UnnamedOrange <lycheng1215@sina.com>
2023-08-04 21:33:28 +0800

    Create empty project

---
+++ source/dll/src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @author UnnamedOrange
+ * @brief DLL entry.
+ * @version 0.0.0
+ * @date 2023-08-04
+ *
+ * @copyright Copyright (c) UnnamedOrange. Licensed under the MIT License.
+ * See the LICENSE file in the repository root for full license text.
+ */
+
+#include <Windows.h>
+
+#include <detours/detours.h>
